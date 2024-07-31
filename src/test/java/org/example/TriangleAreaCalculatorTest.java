ffb8ebd409c6e750709053e87ab1e8d787a2095b
marqosPrado <marcospradofelix@gmail.com>
2024-07-31 10:45:56 -0300

    test: added test for calculate the area of equilateral triangle

--- src/test/java/org/example/TriangleAreaCalculatorTest.java
+++ src/test/java/org/example/TriangleAreaCalculatorTest.java
@@ -13,6 +13,13 @@ public class TriangleAreaCalculatorTest {
     }
 
     @Test
+    public void shouldCalcAreaEquilateral() {
+        TriangleAreaCalculator calculator = new TriangleAreaCalculator(8, 7);
+        double area = calculator.calcAreaEquilateral(8);
+        Assertions.assertEquals(27.7128, area, 0.0001);
+    }
+
+    @Test
     public void shouldReturnExceptionWithZeroBase() {
         Assertions.assertThrows(
                 IllegalArgumentException.class, () -> new TriangleAreaCalculator(0, 7)
