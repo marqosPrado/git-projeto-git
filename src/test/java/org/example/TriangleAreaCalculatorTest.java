d0a7e50d34aaf434c14425ad92de872bafa6cb35
Marcos Prado <marcospradofelix@gmail.com>
2024-08-08 13:30:52 -0300

    refact: creating separate test file for each triangle

--- src/test/java/org/example/TriangleAreaCalculatorTest.java
+++ src/test/java/org/example/TriangleAreaCalculatorTest.java
@@ -22,39 +22,4 @@ public class TriangleAreaCalculatorTest {
         );
     }
 
-    @Test
-    public void shouldCalcAreaOfIsoscelesTriangle() {
-        CalculateTriangleArea triangle = new IsoscelesTriangle(8, 7);
-        double area = triangle.calculateArea();
-        Assertions.assertEquals(28, area, 0.0001);
-    }
-
-
-    @Test
-    public void shouldReturnExceptionWithZeroBase() {
-        Assertions.assertThrows(
-                IllegalArgumentException.class, () -> new IsoscelesTriangle(0, 7)
-        );
-    }
-
-    @Test
-    public void shouldReturnExceptionWithNegativeBase() {
-        Assertions.assertThrows(
-                IllegalArgumentException.class, () -> new IsoscelesTriangle(-8, 7)
-        );
-    }
-
-    @Test
-    public void shouldReturnExceptionWithZeroHeight() {
-        Assertions.assertThrows(
-                IllegalArgumentException.class, () -> new IsoscelesTriangle(8, 0)
-        );
-    }
-
-    @Test
-    public void shouldReturnExceptionWithNegativeHeight() {
-        Assertions.assertThrows(
-                IllegalArgumentException.class, () -> new IsoscelesTriangle(8, -7)
-        );
-    }
 }
