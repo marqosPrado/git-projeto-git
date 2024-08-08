f2db829c2becffea6f4c07afac7c0651d69fcdd5
Marcos Prado <marcospradofelix@gmail.com>
2024-08-08 13:16:56 -0300

    test: create the test to calculate the area of equilateral triangle

--- src/test/java/org/example/TriangleAreaCalculatorTest.java
+++ src/test/java/org/example/TriangleAreaCalculatorTest.java
@@ -1,23 +1,26 @@
 package org.example;
 
+import org.example.interfaces.CalculateTriangleArea;
+import org.example.triangles.EquilateralTriangle;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TriangleAreaCalculatorTest {
 
     @Test
-    public void shouldCalcArea() {
-        TriangleAreaCalculator calculator = new TriangleAreaCalculator(8, 7);
-        double area = calculator.calcAreaCommonTriangle();
-        Assertions.assertEquals(28, area);
+    public void shouldCalcAreaEquilateralTriangle() {
+        CalculateTriangleArea triangle = new EquilateralTriangle(8);
+        double area = triangle.calculateArea();
+        Assertions.assertEquals(27.7128, area, 0.0001);
     }
 
     @Test
-    public void shouldCalcAreaEquilateral() {
-        TriangleAreaCalculator calculator = new TriangleAreaCalculator(8, 7);
-        double area = calculator.calcAreaEquilateral(8);
-        Assertions.assertEquals(27.7128, area, 0.0001);
+    public void shouldReturnExceptionWithInvalidSide() {
+        Assertions.assertThrows(
+                IllegalArgumentException.class, () -> new EquilateralTriangle(-8)
+        );
     }
+
 
     @Test
     public void shouldReturnExceptionWithZeroBase() {
